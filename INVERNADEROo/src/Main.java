c7cf5367c4125e0383ab7c124589426b7ce72b78
santichiconi <89048583+santichiconi@users.noreply.github.com>
2023-07-07 19:54:31 -0300

    Add files via upload

---
+++ INVERNADEROo/src/Main.java
@@ -0,0 +1,18 @@
+import Entity.Aditivos;
+import Entity.FechaYHorario;
+import Entity.GuardarDatos;
+import Service.AditivosService;
+import Service.Menu;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author chuPac on 24/6/2023.
+ * @project Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Menu().menu(new Menu());
+        System.out.println(" 14");
+    }
+}
